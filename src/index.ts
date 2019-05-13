f261aa2245cbe3b462dbeb4a206129052972e83c
dszymanski <dawid.szymanski@appunite.com>
2019-05-13 14:29:26 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * This function says hello.
+ * @param name Some name to say hello for.
+ * @returns The hello.
+ */
+const sayHello = (name: string = "Haz") => `Hello, ${name}!`;
+
+export default sayHello;
