31d51939aa52a4279e719ad955971808d56e931b
dszymanski <support@joinkingschat.com>
2019-06-03 15:56:13 +0200

    fix: Failed to fetch

--- src/api/token.api.ts
+++ src/api/token.api.ts
@@ -1,36 +1,37 @@
 import { authTokenResponseI, refreshAuthTokenRequestI } from '../interfaces';
 
-export const refreshAuthTokenRequest = async ({
+export const refreshAuthTokenRequest = ({
   options,
 }: {
   options: refreshAuthTokenRequestI;
 }): Promise<authTokenResponseI> => {
-  try {
-    const response = await fetch('https://kc-connect.appunite.com/oauth2/token', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        client_id: options.clientId,
-        grant_Type: 'refresh_token',
-        refresh_token: options.refreshToken,
-      }),
+  return fetch('https://kc-connect.appunite.com/oauth2/token', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      client_id: options.clientId,
+      grant_type: 'refresh_token',
+      refresh_token: options.refreshToken,
+    }),
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json().then(payload => {
+          return {
+            accessToken: payload.access_token,
+            expiresInMillis: payload.expires_in_millis,
+            refreshToken: payload.refresh_token,
+          };
+        });
+      }
+      return Promise.reject(Error('error'));
+    })
+    .catch(error => {
+      return Promise.reject(Error(error.message));
     });
-    if (response.ok) {
-      return response.json().then(payload => {
-        return {
-          accessToken: payload.access_token,
-          expiresInMillis: payload.expires_in_millis,
-          refreshToken: payload.refresh_token,
-        } as authTokenResponseI;
-      });
-    }
-    return Promise.reject(Error('error'));
-  } catch (error) {
-    return Promise.reject(Error(error.message));
-  }
 };
 
 export default {
